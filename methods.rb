5fcac517be3061cd43d96bf03b6db4dc5ff7a999
Brook Nega <brooknega21@gmail.com>
2022-10-30 18:10:05 -0400

    commit tree cadd6ca20b6504214f98864ad3a05b08529788f5

--- methods.rb
+++ methods.rb
@@ -1,1 +1,36 @@
-# Your code here!
+def greet_programmer 
+    puts "Hello, programmer!"
+end
+
+puts greet_programmer
+
+def greet name
+    puts "Hello, #{name}!"
+end
+
+greet "Naureen"
+greet "Jimmy"
+
+def greet_with_default name = "programmer"
+    puts "Hello, #{name}!"
+end
+
+greet_with_default
+
+def add num1,num2
+    return num1 + num2
+end
+
+puts add 1,2
+
+def halve num
+    if num.class != Integer
+        return nil
+    end
+    return num/2
+end
+
+puts halve 4
+
+num = 4
+puts num.class
